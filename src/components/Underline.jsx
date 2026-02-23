9972dd4d7890caee30288edd6649fd0e85a79354
jasonf8080 <your-email@example.com>
2026-02-23 00:54:00 -0500

    Update underline component with dynamic widths

--- src/components/Underline.jsx
+++ src/components/Underline.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const Underline = ({mobileWidth, width}) => {
+const Underline = ({
+  mobileWidth = 100,
+  width = 200,
+  height = 8,
+  className = "",
+}) => {
   return (
-    <div className={`h-2 w-[${mobileWidth}px] md:w-[${width}px] text-underline`}></div>
-  )
-}
+    <div
+      style={{
+        "--w": `${Number(mobileWidth)}px`,
+        "--wMd": `${Number(width)}px`,
+        "--h": `${Number(height)}px`,
+      }}
+      className={`
+        w-[var(--w)]
+        md:w-[var(--wMd)]
+        h-[var(--h)]
+        text-underline
+        ${className}
+      `}
+    />
+  );
+};
 
-export default Underline
+export default Underline;
